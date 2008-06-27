a5cd9a20b6e072df7a8c26e80d427f08f303bd3c
mwgamera <mwgamera@gmail.com>
2008-06-27 22:23:57 +0000

    scheduler_register done

--- scheduler.c
+++ scheduler.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "scheduler.h"
 
 typedef struct scheduler_task {
@@ -12,9 +13,43 @@ typedef struct scheduler_task {
 } scheduler_task_t;
 
 /* Tasks indexes */
-scheduler_task_t**scheduler_tasks_id; /* table indexed by id */
-scheduler_task_t**scheduler_tasks_df; /* table of linked lists indexed by fd */
+static scheduler_task_t **scheduler_tasks_id; /* table indexed by id */
+static scheduler_task_t **scheduler_tasks_fd; /* table of lists indexed by fd */
+static int tasks_id_max = 0;
+static int tasks_fd_max = 0;
 
-short scheduler_register() {
-	return 0;
+/* Register a task */
+short scheduler_register(int fd, void (*fun)(void*), void *dat) {
+
+	/* create a task */
+	scheduler_task_t *tt, *task = malloc(sizeof*task);
+	task->fd = fd;
+	task->handler = fun;
+	task->data = dat;
+	task->next_fd = NULL;
+
+	/* resize indexes */
+	scheduler_tasks_id = realloc( scheduler_tasks_id,
+		++tasks_id_max * sizeof*scheduler_tasks_id );
+	if (tasks_fd_max <= fd) {
+		scheduler_tasks_fd = realloc( scheduler_tasks_fd,
+			(fd+1) * sizeof*scheduler_tasks_fd );
+		while (tasks_fd_max <= fd)
+			scheduler_tasks_fd[tasks_fd_max++] = NULL;
+	}
+
+	/* put task in id index */
+	scheduler_tasks_id[ task->id = tasks_id_max-1 ] = task;
+	
+	/* and descriptors index */
+	if ((tt = scheduler_tasks_fd[fd]) == NULL)
+		scheduler_tasks_fd[fd] = task;
+	else {
+		while (tt->next_fd != NULL)
+			tt = tt->next_fd;
+		tt->next_fd = task;
+	}
+	
+	return task->id;
 }
+
