75b5b610993eb6ef8591e6cfdb3be5d4811704fa
mwgamera <mwgamera@gmail.com>
2010-10-26 02:35:46 +0200

    Made event handling in GUI (user input events, expose is yet to be done)

--- gui.c
+++ gui.c
@@ -1,26 +1,29 @@
 #include "gui.h"
 #include <stdint.h>
 #include <xcb/xcb.h>
-#include <X11/keysymdef.h>
+#include <X11/keysym.h>
 #include <stdlib.h>
 #include <assert.h>
 
 static xcb_connection_t *xc = NULL;
 static xcb_screen_t *xs = NULL;
 static xcb_window_t win;
-static xcb_keysym_t keymap[256]; 
+static xcb_keysym_t keymap[256];
 
 /* Cerate main window with proper attributes */
-static xcb_window_t _main_window(xcb_connection_t *xc, int w, int h) {
+static xcb_window_t _main_window(xcb_connection_t *xc, xcb_screen_t *xs, int w, int h) {
   xcb_window_t window;
   uint32_t values[2];
   xcb_cw_t mask;
+
   window = xcb_generate_id(xc);
+
   mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
   values[0] = xs->black_pixel;
-  values[1] = XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_BUTTON_PRESS
-    | XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_LEAVE_WINDOW
+  values[1] = XCB_EVENT_MASK_KEY_PRESS
+    | XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE
     | XCB_EVENT_MASK_BUTTON_MOTION | XCB_EVENT_MASK_EXPOSURE;
+
   xcb_create_window(xc, xs->root_depth, window, xs->root, 0, 0, w, h, 0, 
       XCB_WINDOW_CLASS_INPUT_OUTPUT, xs->root_visual, mask, values);
   return window;
@@ -30,12 +33,12 @@ static xcb_window_t _main_window(xcb_connection_t *xc, int w, int h) {
 static xcb_keysym_t *_init_keymap(xcb_connection_t *xc) {
   xcb_get_keyboard_mapping_reply_t *reply;
   xcb_keysym_t *syms;
-  int i;
+  int i, j, l;
   reply = xcb_get_keyboard_mapping_reply(xc, xcb_get_keyboard_mapping(xc, 8, 248), NULL);
   syms = xcb_get_keyboard_mapping_keysyms(reply);
-  i = xcb_get_keyboard_mapping_keysyms_length(reply) >> 3;
-  while (i--)
-    keymap[i+8] = syms[i<<3];
+  l = xcb_get_keyboard_mapping_keysyms_length(reply);
+  for (i = 8, j = 0; j < l; j += reply->keysyms_per_keycode, i++)
+    keymap[i] = syms[j];
   free(reply);
   return keymap;
 }
@@ -46,17 +49,18 @@ int gui_init(int w, int h) {
   xc = xcb_connect(NULL, NULL);
   if (xcb_connection_has_error(xc)) return -1;
   xs = xcb_setup_roots_iterator(xcb_get_setup(xc)).data;
-  win = _main_window(xc, w, h);
+  win = _main_window(xc, xs, w, h);
   xcb_map_window(xc, win);
   _init_keymap(xc);
   return 0;
 }
 
-#include <stdio.h> /*FIXME printf */
+
+#include <stdio.h> /*FIXME DEBUG: printf */
 /* Close and clean up */
 int gui_fin() {
   assert(xc);
-  fprintf(stderr,"gui_fin\n");
+  fprintf(stderr,"gui_fin\n"); /*FIXME DEBUG*/
   xcb_disconnect(xc);
   xc = NULL;
   return 0;
@@ -68,31 +72,178 @@ int gui_fd() {
   return xcb_get_file_descriptor(xc);
 }
 
-#include <unistd.h> /* FIXME sleep */
-/* Process events, return request for higher layer */
-gui_event_t gui_poll() {
-  xcb_generic_event_t *e;
+/* Flush data to server */
+void gui_update() {
   assert(xc);
-  while ((e = xcb_poll_for_event(xc)) != NULL) {
+  xcb_flush(xc);
+}
+
+
+#define DIVSQRT2(x) (((x)*46341)>>16) /* ~= x / sqrt(2) */
+
+/* Handle keyboard events */
+static gui_event_t _event_kbd(xcb_keycode_t code) {
+  gui_event_t r = { GUI_EVENT_NONE, {0, 0, 0} };
+  assert(code >= 8);
+  printf("scancode: %d, keysym:0x%04x\n", code, keymap[code]); /*FIXME:DEBUG*/
+  switch (keymap[code]) {
+    /* horizontal movement (WSAD/arrows) */
+    case XK_w: case XK_Up:
+      r.type = GUI_EVENT_FORWARD;
+      r.scale[0] = +GUI_SCALE_KBD;
+      break;
+    case XK_s: case XK_Down:
+      r.type = GUI_EVENT_FORWARD;
+      r.scale[0] = -GUI_SCALE_KBD;
+      break;
+    case XK_a: case XK_Left:
+      r.type = GUI_EVENT_RIGHT;
+      r.scale[0] = -GUI_SCALE_KBD;
+      break;
+    case XK_d: case XK_Right:
+      r.type = GUI_EVENT_RIGHT;
+      r.scale[0] = +GUI_SCALE_KBD;
+      break;
+    /* pitch/yaw rotations (HJKL+YUBN) */
+    case XK_k:
+      r.type = GUI_EVENT_PITCH;
+      r.scale[0] = +GUI_SCALE_KBD;
+      break;
+    case XK_j:
+      r.type = GUI_EVENT_PITCH;
+      r.scale[0] = -GUI_SCALE_KBD;
+      break;
+    case XK_h:
+      r.type = GUI_EVENT_YAW;
+      r.scale[0] = -GUI_SCALE_KBD;
+      break;
+    case XK_l:
+      r.type = GUI_EVENT_YAW;
+      r.scale[0] = +GUI_SCALE_KBD;
+      break;
+    case XK_y:
+      r.type = GUI_EVENT_PITCH | GUI_EVENT_YAW;
+      r.scale[0] = +DIVSQRT2(GUI_SCALE_KBD);
+      r.scale[1] = -DIVSQRT2(GUI_SCALE_KBD);
+      break;
+    case XK_u:
+      r.type = GUI_EVENT_PITCH | GUI_EVENT_YAW;
+      r.scale[0] = +DIVSQRT2(GUI_SCALE_KBD);
+      r.scale[1] = +DIVSQRT2(GUI_SCALE_KBD);
+      break;
+    case XK_b:
+      r.type = GUI_EVENT_PITCH | GUI_EVENT_YAW;
+      r.scale[0] = -DIVSQRT2(GUI_SCALE_KBD);
+      r.scale[1] = -DIVSQRT2(GUI_SCALE_KBD);
+      break;
+    case XK_n:
+      r.type = GUI_EVENT_PITCH | GUI_EVENT_YAW;
+      r.scale[0] = -DIVSQRT2(GUI_SCALE_KBD);
+      r.scale[1] = +DIVSQRT2(GUI_SCALE_KBD);
+      break;
+    /* vertical movement (TG) */
+    case XK_t:
+      r.type = GUI_EVENT_DOWN;
+      r.scale[0] = -GUI_SCALE_KBD;
+      break;
+    case XK_g:
+      r.type = GUI_EVENT_DOWN;
+      r.scale[0] = +GUI_SCALE_KBD;
+      break;
+    /* rolls (<>) */
+    case XK_comma:
+      r.type = GUI_EVENT_ROLL;
+      r.scale[0] = -GUI_SCALE_KBD;
+      break;
+    case XK_period:
+      r.type = GUI_EVENT_ROLL;
+      r.scale[0] = +GUI_SCALE_KBD;
+      break;
+    /* focus */
+    case XK_plus: case XK_equal: case XK_KP_Add:
+      r.type = GUI_EVENT_FOCUS;
+      r.scale[0] = +GUI_SCALE_KBD;
+      break;
+    case XK_minus: case XK_KP_Subtract:
+      r.type = GUI_EVENT_FOCUS;
+      r.scale[0] = -GUI_SCALE_KBD;
+      break;
+    /* quit */
+    case XK_Escape:
+      r.type = GUI_EVENT_QUIT;
+      break;
+    default:
+      break;
+  }
+  return r;
+}
+
+#define MODIFIER_BUTTON1 0x100
+#define MODIFIER_BUTTON2 0x200
+#define MODIFIER_BUTTON3 0x400
+
+/* Handle mouse events */
+static gui_event_t _event_ptr(xcb_motion_notify_event_t *e) {
+  static int16_t last_x, last_y;
+  gui_event_t r = { GUI_EVENT_NONE, { 0, 0, 0 } };
+  int k, i = 0;
+  if (e->state & MODIFIER_BUTTON3) { /* right button - planar movement */
+    k = (last_y - e->event_y) * GUI_SCALE_PRT;
+    if (k) { r.type |= GUI_EVENT_FORWARD; r.scale[i++] = k; }
+    k = (e->event_x - last_x) * GUI_SCALE_PRT;
+    if (k) { r.type |= GUI_EVENT_RIGHT; r.scale[i++] = k; }
+  } else
+  if (e->state & MODIFIER_BUTTON1) { /* left button - phi,theta rotation */
+    k = (last_y - e->event_y) * GUI_SCALE_PRT;
+    if (k) { r.type |= GUI_EVENT_PITCH; r.scale[i++] = k; }
+    k = (e->event_x - last_x) * GUI_SCALE_PRT;
+    if (k) { r.type |= GUI_EVENT_YAW; r.scale[i++] = k; }
+  }
+  if (e->state & MODIFIER_BUTTON2) { /* middle button - focal length */
+    r.type |= GUI_EVENT_FOCUS;
+    r.scale[i++] = (e->event_y - last_y) * GUI_SCALE_PRT;
+  }
+  assert(i <= 4);
+  last_x = e->event_x;
+  last_y = e->event_y;
+  return r;
+}
+
+/* Process events, return requests for higher layer */
+gui_event_t gui_poll() {
+  xcb_generic_event_t *e = NULL;
+  gui_event_t r = { GUI_EVENT_NONE, { 0, 0, 0 } };
+  assert(xc);
+  if (xcb_connection_has_error(xc)) {
+    r.type = GUI_EVENT_QUIT;
+    return r;
+  }
+  while (!r.type && (e = xcb_poll_for_event(xc)) != NULL) {
     switch (e->response_type & ~0x80) {
-      case XCB_KEY_PRESS:
-        {
-        xcb_key_press_event_t *kp = (xcb_key_press_event_t*) e;
-        printf("Key %d -> %d '%c'\n", kp->detail, keymap[kp->detail], keymap[kp->detail]);
+      /* navigation with pointing device */
+      case XCB_MOTION_NOTIFY:
+        r = _event_ptr((xcb_motion_notify_event_t*)e);
         break;
-        }
+      case XCB_BUTTON_PRESS:
+        _event_ptr((xcb_motion_notify_event_t*)e);
+        xcb_flush(xc);
+        break;
+      /* keyboard navigation */
+      case XCB_KEY_PRESS:
+        r = _event_kbd(((xcb_key_press_event_t*)e)->detail);
+        break;
+      /* events handled internally */
       case XCB_EXPOSE:
-        printf("Expose\n");
+        printf("Expose\n"); /* TODO */
+        xcb_flush(xc);
         break;
       default:
         break;
     }
     free(e);
   }
-  if (xcb_connection_has_error(xc))
-    return GUI_EVENT_INVALID;
-  xcb_flush(xc);
-  /* TODO Somehow returning something other than INVALID yields leaks --investigate */
-  return GUI_EVENT_NONE;
+  if (!r.type && xcb_connection_has_error(xc))
+    r.type = GUI_EVENT_QUIT;
+  return r;
 }
 
--- gui.h
+++ gui.h
@@ -6,24 +6,42 @@
 int gui_init(int width, int height);
 int gui_fin(void);
 
+/* Update GUI, should be called after doing any changes */
+void gui_update(void);
+
 /* Get a descriptor to select/poll on */
 int gui_fd(void);
 
 /* Event type */
 typedef enum {
-  /* no events available */
+  /* nothing happens */
   GUI_EVENT_NONE = 0,
   /* movements (observer frame) */
-  GUI_EVENT_FWD,  GUI_EVENT_BACK,
-  GUI_EVENT_LEFT, GUI_EVENT_RIGHT,
-  GUI_EVENT_UP,   GUI_EVENT_DOWN,
+  GUI_EVENT_FORWARD = 0x001,
+  GUI_EVENT_RIGHT   = 0x002,
+  GUI_EVENT_DOWN    = 0x004,
   /* rotations (observer frame, RPY as for aircrafts) */
-  GUI_EVENT_ROLL_CCW,   GUI_EVENT_ROLL_CW,
-  GUI_EVENT_PITCH_DOWN, GUI_EVENT_PITCH_UP,
-  GUI_EVENT_YAW_LEFT,   GUI_EVENT_YAW_RIGHT,
-  /* error condition */
-  GUI_EVENT_INVALID = -1
+  GUI_EVENT_ROLL  = 0x010, /* clockwise */
+  GUI_EVENT_PITCH = 0x020, /* look up */
+  GUI_EVENT_YAW   = 0x040, /* look right */
+  /* zoom (changes in focal length) */
+  GUI_EVENT_FOCUS = 0x100,
+  /* quit requested by user or caused by error condition */
+  GUI_EVENT_QUIT = 0x8000
+} gui_event_type_t;
+
+/* Event structure */
+/* From controller's point of view, events are user's request to change some
+ * properties of camera.  Multiple changes may be requested in single event but
+ * GUI should be built in a way that makes composed events always defined (for
+ * example simultaneous pitch and yaw are okay) */
+typedef struct {
+  gui_event_type_t type; /* directions of changes (bit mask) */
+  unsigned scale[3]; /* scale for each of change types (one for each bit, max 3) */
 } gui_event_t;
+
+#define GUI_SCALE_KBD 50
+#define GUI_SCALE_PRT 1
 
 /* Get next available event for controller */
 gui_event_t gui_poll(void);
