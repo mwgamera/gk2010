a4cc430c077aeb868c84d57c6d2fc770dc20d0e0
mwgamera <mwgamera@gmail.com>
2010-11-22 17:58:54 +0100

    Rage. Rage. Rage. Is this workin'?

--- main.c
+++ main.c
@@ -2,57 +2,73 @@
 #include "scheduler.h"
 #include "space.h"
 #include "camera.h"
+#include "model.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-/* Projective line drawing */
-void draw_line(tmatrix p, point a, point b) {
-  int x0, y0, x1, y1;
-  a = transform(p, a);
-  b = transform(p, b);
-  if (POINT_GET(a,2) < 0 || POINT_GET(b,2) < 0) return;
-  a = normalize(a);
-  b = normalize(b);
-  x0 = POINT_GET(a, 0); y0 = POINT_GET(a, 1);
-  x1 = POINT_GET(b, 0); y1 = POINT_GET(b, 1);
-  gui_draw_line(x0,y0,x1,y1);
+int nobjects = 0;
+model **object = NULL;
+
+/* Load scene */
+void make_scene(void) {
+  static char *fns[] = { "cube1.model" };
+  FILE *fp;
+  int i, n = (sizeof fns / sizeof *fns);
+  assert(nobjects == 0);
+  object = malloc(n * sizeof*object);
+  if (object == NULL) {
+    fprintf(stderr, "malloc error\n");
+    return;
+  }
+  for (i = 0; i < n; i++) {
+    if ((fp = fopen(fns[i],"rb")) != NULL) {
+      object[nobjects++] = model_read(fp);
+      fclose(fp);
+    }
+    else
+      fprintf(stderr, "fopen error : %s\n", fns[nobjects]);
+  }
+  assert(nobjects <= i);
+  assert(nobjects > 0);
 }
 
-/* Redraw */
-void draw_scene() {
+/* Clean up scene */
+void clean_scene(void) {
+  assert(nobjects > 0);
+  while (nobjects--)
+    model_free(object[nobjects]);
+  free(object);
+}
+
+/* Draw wire-frame without occlusions */
+void draw_scene(void) {
+  int i, k;
   tmatrix p = camera_transform();
+  for (i = 0; i < nobjects; i++)
+    model_transform(p, object[i]);
   gui_clear();
-  { /* FIXME: Example cube */
-    point cube[][4] = {
-      { POINT( 0, 0, 0), POINT(60, 0, 0), POINT(60,60, 0), POINT( 0,60, 0) },
-      { POINT( 0, 0,60), POINT(60, 0,60), POINT(60,60,60), POINT( 0,60,60) }
-    };
-    tmatrix w = TMATRIX(1,0,0,0, 0,1,0,330, 0,0,1,170, 0,0,0,1);
-    int i;
-    w = tcompose(p, w);
-    for (i = 0; i < 4; i++) draw_line(w, cube[0][i], cube[0][(i+1)%4]);
-    for (i = 0; i < 4; i++) draw_line(w, cube[1][i], cube[1][(i+1)%4]);
-    for (i = 0; i < 4; i++) draw_line(w, cube[0][i], cube[1][i]);
-  }
-  { /* FIXME: Floor grid */
-    int i, j;
-    tmatrix w = TMATRIX(1,0,0,0, 0,1,0,360, 0,0,1,260, 0,0,0,1);
-    w = tcompose(p, w);
-    for (i = -10; i < 10; i++)
-      for (j = -10; j < 10; j++) {
-        point a = POINT((i+0)*50, (j+0)*50, 0);
-        point b = POINT((i+0)*50, (j+1)*50, 0);
-        point c = POINT((i+1)*50, (j+1)*50, 0);
-        draw_line(w, a, b);
-        draw_line(w, b, c);
-      }
+  for (i = 0; i < nobjects; i++) {
+    /* wire-frame specific code, relies on structure of model */
+    for (k = 0; k < object[i]->nedges; k++) {
+      point a = object[i]->pvertex[object[i]->edge[(k<<1)+0]];
+      point b = object[i]->pvertex[object[i]->edge[(k<<1)+1]];
+      if (POINT_GET(a,2) >= 0 && POINT_GET(b,2) >= 0)
+        gui_draw_line(
+            POINT_GET(a,0),
+            POINT_GET(a,1),
+            POINT_GET(b,0),
+            POINT_GET(b,1));
+    }
   }
   gui_update();
 }
 
 /* Event loop */
 void loop(void *x) {
-  int i, k = 0;
+  int k = 0;
   gui_event_t ev = gui_poll();
+  x = NULL;
   if (!ev.type) return;
 
   if (ev.type & GUI_EVENT_FORWARD) CAMERA_FORWARD(ev.scale[k++]);
@@ -72,10 +88,12 @@ void loop(void *x) {
 /* Main */
 int main() {
   gui_init(640, 480);
+  make_scene();
   camera_reset(640, 480, 1);
   draw_scene();
   scheduler_register(gui_fd(), SCHEDULER_FD_READ, &loop, NULL);
   scheduler_main();
   gui_fin();
+  clean_scene();
   return 0;
 }
